a15e9f13f08298e4e582151ae2cea7a8f1ed0dcd
supertick <supertick@gmail.com>
2025-01-12 13:03:59 -0800

    tags

--- apps/ai_core/src/ai_core/main.py
+++ apps/ai_core/src/ai_core/main.py
@@ -4,16 +4,16 @@ app = FastAPI()
 
 # Import and include routers
 from .api.config_api import router as config_router
-app.include_router(config_router, prefix='/config')
+app.include_router(config_router, prefix='/v1', tags=["Config"])
 from .api.company_api import router as company_router
-app.include_router(company_router, prefix='/company')
+app.include_router(company_router, prefix='/v1', tags=["Company"])
 from .api.user_api import router as user_router
-app.include_router(user_router, prefix='/user')
+app.include_router(user_router, prefix='/v1', tags=["User"])
 from .api.role_api import router as role_router
-app.include_router(role_router, prefix='/role')
+app.include_router(role_router, prefix='/v1', tags=["Role"])
 from .api.transcription_request_api import router as transcription_request_router
-app.include_router(transcription_request_router, prefix='/transcription_request')
+app.include_router(transcription_request_router, prefix='/v1', tags=["Transcription Request"])
 from .api.transcription_api import router as transcription_router
-app.include_router(transcription_router, prefix='/transcription')
+app.include_router(transcription_router, prefix='/v1', tags=["Transcription"])
 from .api.transcription_result_api import router as transcription_result_router
-app.include_router(transcription_result_router, prefix='/transcription_result')
+app.include_router(transcription_result_router, prefix='/v1', tags=["Transcription Result"])
